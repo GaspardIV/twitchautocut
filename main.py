0d13eaa7a1fd45cd45a795e4858a6e4a751f6ea8
GaspardIV <otto.czwarty@gmail.com>
2019-11-13 13:40:30 +0100

    download working -> display chat messages over time :)

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import sys
-from rechatdl import download_from_twitch
-download_from_twitch("506496104")
+from rechatdl import download_messages_from_twitch, download_vid_info
+
+vid_id = "506496104"
+vid_info = download_vid_info(vid_id)
+messages = download_messages_from_twitch(vid_id)
+print(vid_info)
+for message in messages:
+    print(message["commenter"]["display_name"] + ": " + message["message"]["body"])
+# print(messages)
--- rechatdl.py
+++ rechatdl.py
@@ -7,53 +7,15 @@ import time
 import math
 import json
 
-def download_from_twitch(void, file_name=None):
+CHUNK_ATTEMPTS = 6
+CHUNK_ATTEMPT_SLEEP = 10
+CLIENT_ID = "isaxc3wjcarzh4vgvz11cslcthw0gw"
+
+
+def download_from_twitch_to_file(vid_id, file_name=None):
     if file_name is None:
-        file_name = "vid_" + void
-    CHUNK_ATTEMPTS = 6
-    CHUNK_ATTEMPT_SLEEP = 10
-        
-    messages = []
-
-    cid = "isaxc3wjcarzh4vgvz11cslcthw0gw"
-    vod_info = requests.get("https://api.twitch.tv/kraken/videos/v" + void, headers={"Client-ID": cid, "Accept": "application/vnd.twitchtv.v5+json"}).json()
-
-    if "error" in vod_info:
-        sys.exit("got an error in vod info response: " + str(vod_info))
-
-    messages.append(vod_info)   # we store the vod metadata in the first element of the message array
-
-    response = None
-
-    print("downloading chat messages for vod " + void + "...")
-    while response == None or '_next' in response:
-        query = ('cursor=' + response['_next']) if response != None and '_next' in response else 'content_offset_seconds=0'
-        for i in range(0, CHUNK_ATTEMPTS):
-            error = None
-            try:
-                response = requests.get("https://api.twitch.tv/v5/videos/" + void + "/comments?" + query, headers={"Client-ID": cid}).json()
-            except requests.exceptions.ConnectionError as e:
-                error = str(e)
-            else:
-                if "errors" in response or not "comments" in response:
-                    error = "error received in chat message response: " + str(response)
-            
-            if error == None:
-                messages += response["comments"]
-                break
-            else:
-                print("\nerror while downloading chunk: " + error)
-                
-                if i < CHUNK_ATTEMPTS - 1:
-                        print("retrying in " + str(CHUNK_ATTEMPT_SLEEP) + " seconds ", end="")
-                print("(attempt " + str(i + 1) + "/" + str(CHUNK_ATTEMPTS) + ")")
-                
-                if i < CHUNK_ATTEMPTS - 1:
-                    time.sleep(CHUNK_ATTEMPT_SLEEP)
-        
-        if error != None:
-            sys.exit("max retries exceeded.")
-
+        file_name = "vid_" + vid_id
+    messages = download_messages_from_twitch(vid_id)
     print()
     print("saving to " + file_name)
 
@@ -62,3 +24,49 @@ def download_from_twitch(void, file_name=None):
     f.close()
 
     print("done!")
+
+
+def download_vid_info(vid_id):
+    print("downloading vid info for vod " + vid_id + "...")
+    vid_info = requests.get("https://api.twitch.tv/kraken/videos/v" + vid_id,
+                            headers={"Client-ID": CLIENT_ID, "Accept": "application/vnd.twitchtv.v5+json"}).json()
+    if "error" in vid_info:
+        sys.exit("got an error in vod info response: " + str(vid_info))
+    print("done")
+    return vid_info
+
+
+def download_messages_from_twitch(vid_id):
+    messages = []
+    response = None
+    print("downloading chat messages for vod " + vid_id + "...")
+    while response is None or '_next' in response:
+        query = ('cursor=' + response[
+            '_next']) if response is not None and '_next' in response else 'content_offset_seconds=0'
+
+        for i in range(0, CHUNK_ATTEMPTS):
+            error = None
+            try:
+                response = requests.get("https://api.twitch.tv/v5/videos/" + vid_id + "/comments?" + query,
+                                        headers={"Client-ID": CLIENT_ID}).json()
+            except requests.exceptions.ConnectionError as e:
+                error = str(e)
+            else:
+                if "errors" in response or "comments" not in response:
+                    error = "error received in chat message response: " + str(response)
+
+            if error is None:
+                messages += response["comments"]
+                break
+            else:
+                print("\nerror while downloading chunk: " + error)
+
+                if i < CHUNK_ATTEMPTS - 1:
+                    print("retrying in " + str(CHUNK_ATTEMPT_SLEEP) + " seconds ", end="")
+                    print("(attempt " + str(i + 1) + "/" + str(CHUNK_ATTEMPTS) + ")")
+                    time.sleep(CHUNK_ATTEMPT_SLEEP)
+                else:
+                    sys.exit("max retries exceeded.")
+
+    print("done!")
+    return messages
