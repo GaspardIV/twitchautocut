cd1ecd967c94bb9a9572ccdc9ba4090bb84da89c
GaspardIV <kacper.konecki@gmail.com>
2019-12-06 01:43:11 +0100

    nice plot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 from main_download import download_info_and_messages_to_files
-import matplotlib
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # download_info_and_messages_to_files()
 
@@ -9,9 +11,13 @@ m_created = []
 with open('506496104__messages.txt') as json_file:
     messages = json.load(json_file)
     for message in messages:
-        # print(message["commenter"]["display_name"] + "\n\t " + message["message"]["body"] + "\n")
         m_bodies.append(message["message"]["body"])
         m_created.append(message["created_at"])
-
-
-
+df = pd.DataFrame({"time": m_created, "mess": m_bodies})
+df.time = pd.to_datetime(df['time'], format='%Y-%m-%d %H:%M:%S.%f')
+df.set_index(["time"], inplace=True)
+print(df.index)
+df.groupby([df.index.year, df.index.month, df.index.day, df.index.hour, df.index.minute, df.index.second]).count().plot(kind="bar")
+plt.show()
+print(df.head())
+print(df.tail())
