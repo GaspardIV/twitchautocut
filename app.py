c0c4d3986817279299a8779df9cc86ada04d764e
gaspardIV <otto.czwarty@gmail.com>
2020-04-12 18:26:19 +0200

    example with mask

--- app.py
+++ app.py
@@ -1,46 +1,38 @@
-import pyglet
 from moviepy.editor import *
-pyglet.lib.load_library('avbin')
-pyglet.have_avbin=True
-
+import cv2
 
 video = VideoFileClip("Twitch.mp4")
-v1 = video.subclip(745,761)
-v2 = video.subclip(12352, 12368)
+# v1 = video.subclip(745,761)
+# v2 = video.subclip(12352, 12368)
 # result = CompositeVideoClip([video, txt_clip]) # Overlay text on video
-v2.write_videofile("out.mp4") # Many options...
-v1.write_videofile("out1.mp4") # Many options...
+# v2.write_videofile("out.mp4") # Many options...
+# v1.write_videofile("out1.mp4") # Many options...
 
-vidPath = 'out.mp4'
+capture = cv2.VideoCapture("Twitch.mp4")
 
-window = pyglet.window.Window()
-player = pyglet.media.Player()
+while True:
+    ret, frame = capture.read()
+    if frame is None:
+        break
+    backSub = cv2.createBackgroundSubtractorKNN() #|| cv2.createBackgroundSubtractorMOG2()
+    fgMask = backSub.apply(frame)
+
+    cv2.rectangle(frame, (10, 2), (100, 20), (255, 255, 255), -1)
+    cv2.putText(frame, str(capture.get(cv2.CAP_PROP_POS_FRAMES)), (15, 15),
+               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+
+    cv2.imshow('Frame', frame)
+    cv2.imshow('FG Mask', fgMask)
+
+    keyboard = cv2.waitKey(30)
+    if keyboard == 'q' or keyboard == 27:
+        break
+
+
+# vidPath = 'out.mp4'
+
 # source = pyglet.media.StreamingSource()
 # MediaLoad = pyglet.media.load(vidPath)
 # # need ffmpeg codec http://blog.gregzaal.com/how-to-install-ffmpeg-on-windows/ and avbin on windows!!
 # player.queue(MediaLoad)
 # player.play()
-
-@window.event
-def on_draw():
-    # window.clear()
-    # label.draw()
-    if player.source and player.source.video_format:
-        player.get_texture().blit(50, 50)
-
-@window.event
-def on_key_press(symbol, modifiers):
-    if (65288, 16) == (symbol, modifiers):
-        print("backspace")
-    if (65293, 16) == (symbol, modifiers):
-        print("enter")
-    if (65361, 16) == (symbol, modifiers):
-        print("left")
-    if (65363, 16) == (symbol, modifiers):
-        print("right")
-    if (65361, 18) == (symbol, modifiers):
-        print("ctrl left")
-    if (65363, 18) == (symbol, modifiers):
-        print("ctrl right")
-
-pyglet.app.run()
