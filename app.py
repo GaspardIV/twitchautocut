97173435810bd818048e9c0f1e3662c1a24cf77b
gaspardIV <otto.czwarty@gmail.com>
2020-04-16 23:08:36 +0200

    czystrzy kod ale 5 od 6 nie odroznia jeszcze

--- app.py
+++ app.py
@@ -31,13 +31,11 @@ def every_n_frame(file, n):
         i %= n
 
 
-def recognizedigit(src):
-    # cdst = cv2.cvtColor(src, cv2.COLOR_GRAY2BGR)
-    # global knn
+def recognizedigit(src, knn):
     arr = [get_feature(src)]
     arr = np.array(arr, np.float32)
     ret, result, neighbours, dist = knn.findNearest(arr, k=5)
-    return result[0]
+    return labeltodigit(result[0])
 
 
 def get_score_area(frame):
@@ -58,98 +56,89 @@ def get_letters_bounding(score_gray):
         parent = hierarchy[0][i][3]
         if parent != -1 and hierarchy[0][parent][3] == -1:  # it has parent, but does not have a grandpa
             bound_rect.append(cv2.boundingRect(contours_poly[i]))
-
     bound_rect = sorted(bound_rect)
-    for i in range(1, len(bound_rect)):  # check if y's are the same
-        if not abs(bound_rect[i][1] - bound_rect[i - 1][1]) < 5 or not abs(bound_rect[i][3] - bound_rect[i - 1][3]) < 5:
-            return None
     return bound_rect
 
 
-def get_score_from_frame(frame):
+def boundings_fine(bound_rect):
+    for i in range(1, len(bound_rect)):  # check if y's are the same
+        if not abs(bound_rect[i][1] - bound_rect[i - 1][1]) < 5 or not abs(bound_rect[i][3] - bound_rect[i - 1][3]) < 5:
+            return False
+    return True
+
+
+def get_letters(frame):
+    letters = []
     score_gray = get_score_area(frame)
     bound_rect = get_letters_bounding(score_gray)
-
-    if bound_rect:
-        cv2.imshow('aa', score_gray)
-
-    for rect in bound_rect:
-        letterimage = score_gray[rect[1]:rect[1] + rect[3], rect[0]:rect[0] + rect[2]]
-        letter = recognizedigit(letterimage)
-        print(letter, end="")
-    print("")
-    # cv2.imshow("aa", score_area)
-    # if text.count("/") == 2:
-    #     kills, deads, assists = text.split("/")
-    #     if kills.isdigit() and deads.isdigit() and assists.isdigit():
-    #         return int(kills), int(deads), int(assists)
-    # return None
-
-
-digits = []
-labels = []
+    if boundings_fine(bound_rect):
+        # cv2.imshow('aa', score_gray)
+        for rect in bound_rect:
+            letterimage = score_gray[rect[1]:rect[1] + rect[3], rect[0]:rect[0] + rect[2]]
+            letters.append(letterimage)
+        return letters
 
 
 def get_feature(image):
-    # bordered = np.zeros((20, 20), np.float32)
     bordered = np.zeros([20, 20], dtype=np.uint8)
     bordered.fill(255)
-    # image2 = cv2.resize(image, (10, 30), interpolation=cv2.INTER_LINEAR)
     bordered[0:image.shape[0], 0:image.shape[1]] = image
-    # todo moze zamiast rozciafgania dopelnianie do krztałtu
-    # cv2.imshow("xd", bordered)
-    # cv2.imshow("xd2", image)
-    # cv2.waitKey(1000)
     ret = bordered.astype(np.float32)
     ret = 255 - ret
     return ret.ravel()
 
 
-# for i in range(10):
-#     image = cv2.imread('train_data/{}.jpg'.format(i))
-#     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#     digits.append(get_feature(image))
-#     labels.append(i)
-# td = cv2.imread('train_data/_.jpg')
-# td = cv2.cvtColor(td, cv2.COLOR_BGR2GRAY)
-# digits.append(get_feature(td))
-# labels.append(10)
-
-
-digits = []
-labels = []
-
-
-def addFileData(file, label):
+def addFileData(file):
     image = cv2.imread(file)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    digits.append(get_feature(image))
-    labels.append(label)
+    return get_feature(image)
 
 
-def readFolder(folder_name, label):
+def readFolder(folder_name):
+    res = []
     for file in os.listdir(folder_name):
-        addFileData(folder_name + file, label)
+        res.append(addFileData(folder_name + file))
+    return res
 
 
-readFolder("train_data/0/", 0)
-readFolder("train_data/1/", 1)
-readFolder("train_data/2/", 2)
-readFolder("train_data/3/", 3)
-readFolder("train_data/4/", 4)
-readFolder("train_data/5/", 5)
-readFolder("train_data/6/", 6)
-readFolder("train_data/7/", 7)
-readFolder("train_data/8/", 8)
-readFolder("train_data/9/", 9)
-readFolder("train_data/slash/", 10)
+def train():
+    digits = []
+    labels = []
+    for i in range(11):
+        dirdigits = readFolder("train_data/{}/".format(i))
+        digits.extend(dirdigits)
+        labels.extend([i] * len(dirdigits))
+    digits = np.array(digits, np.float32)
+    labels = np.array(labels, np.float32)
+    knn = cv2.ml.KNearest_create()
+    knn.train(digits, cv2.ml.ROW_SAMPLE, labels)
+    return knn
 
-knn = cv2.ml.KNearest_create()
-digits = np.array(digits, np.float32)
-labels = np.array(labels, np.float32)
-knn.train(digits, cv2.ml.ROW_SAMPLE, labels)
 
-for f_name in ["out.mp4", "8.mp4", "56.mp4", "349.mp4"]:
-    for frame, frame_time in every_n_frame(f_name, 60):
-        get_score_from_frame(frame)
-        # key = cv2.waitKey(1000)
+def labeltodigit(label):
+    return {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 10: "/"}[label[0]]
+
+
+def recognizeScore(letters, knn):
+    score = ''.join([recognizedigit(letter, knn) for letter in letters])
+    if score.count("/") == 2:
+        kills, deads, assists = score.split("/")
+        return int(kills), int(deads), int(assists)
+    return None
+
+
+
+if __name__ == '__main__':
+    knn = train()
+    # for f_name in ["out.mp4", "8.mp4", "56.mp4", "349.mp4"]:
+    for f_name in ["56.mp4"]:
+        for frame, frame_time in every_n_frame(f_name, 5):
+            letters = get_letters(frame)
+            if letters:
+                kills, deaths, assists = recognizeScore(letters, knn)
+                print(kills, deaths, assists)
+
+                cv2.imshow("xd", frame)
+                cv2.waitKey(30)
+
+# todo 5 od 6 nie odroznia
